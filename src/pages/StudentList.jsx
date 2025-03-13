eaf051956789966c044f016e5c4b4897ac729b9e
Trần Thái Nhật <155871440+22120255@users.noreply.github.com>
2025-03-13 12:21:24 +0700

    Revert "Backup for main"

--- src/pages/StudentList.jsx
+++ src/pages/StudentList.jsx
@@ -1,14 +1,19 @@
 import "./StudentManagement.css";
 import PropTypes from "prop-types";
 
-function StudentList({ students }) {
-  const handleDeleteStudent = (studentName) => {
-    const isConfirmed = window.confirm(`Bạn có chắc chắn muốn xóa ${studentName}?`);
-    if (isConfirmed) {
-      alert(`Đã xóa ${studentName} thành công!`);
-      // TODO: Gọi API để xóa sinh viên
-    }
-  };
+function StudentTable({ students }) {
+  if (students.length === 0) {
+    return (
+      <div className="empty-list">
+        <img
+          src="/images/list_empty.png"
+          alt="empty"
+          className="empty-list__img"
+        />
+        <p className="empty-list__title">Không có thông tin sinh viên nào!</p>
+      </div>
+    );
+  }
 
   return (
     <div className="student-table">
@@ -18,37 +23,30 @@ function StudentList({ students }) {
             <th>Name</th>
             <th>Student ID</th>
             <th>Email address</th>
-            <th>Phone</th>
+            <th>Khoa</th>
             <th>Gender</th>
-            <th>Note</th>
-            <th>Actions</th>
+            <th>Ghi chú</th>
           </tr>
         </thead>
         <tbody>
-          {students.map((student) => (
-            <tr key={student.student_code} className="highlight-row">
+          {students.map((student, index) => (
+            <tr
+              key={index}
+              className={index === students.length - 1 ? "highlight-row" : ""}
+            >
               <td className="student-name">
-              <button className="student-avatar">
-              {student.fullname.split(" ").map(word => word[0]).join("").toUpperCase()}
-            </button>
-                {student.fullname}
+                <img
+                  src="/images/avatar.png"
+                  alt="avatar"
+                  className="student-avatar"
+                />
+                {student.name}
               </td>
-              <td>{student.student_code}</td>
+              <td>{student.id}</td>
               <td>{student.email}</td>
-              <td>{student.phone}</td>
+              <td>{student.khoa}</td>
               <td>{student.gender}</td>
               <td></td>
-              <td>
-                <button className="student-edit">
-                <i className='bx bx-show'></i>
-                </button>
-                <button className="student-edit">
-                  <i className="bx bx-message-square-edit"></i>
-                </button>
-                <button onClick={() => handleDeleteStudent(student.fullname)} className="student-delete">
-                  <i className="bx bx-message-square-x"></i>
-                </button>
-              </td>
             </tr>
           ))}
         </tbody>
@@ -57,15 +55,17 @@ function StudentList({ students }) {
   );
 }
 
-StudentList.propTypes = {
+StudentTable.propTypes = {
   students: PropTypes.arrayOf(
     PropTypes.shape({
-      student_code: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-      fullname: PropTypes.string.isRequired,
-      email: PropTypes.string,
-      phone: PropTypes.string,
-      gender: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+      khoa: PropTypes.string.isRequired,
+      gender: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
-export default StudentList;
+
+
+export default StudentTable;
