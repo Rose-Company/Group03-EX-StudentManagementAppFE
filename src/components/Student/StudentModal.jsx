ff245665828b67961dc742196c991cb8cc70ae87
22120255 <22120255@student.hcmus.edu.vn>
2025-03-19 00:07:28 +0700

    SA-31-fix-something

--- src/components/Student/StudentModal.jsx
+++ src/components/Student/StudentModal.jsx
@@ -100,7 +100,8 @@ const StudentModal = ({
   };
 
   const handleFieldClick = (e) => {
-    e.stopPropagation(); // Ngăn sự kiện lan ra ngoài
+    e.preventDefault();
+    e.stopPropagation();
     if (!isEditing) {
       setIsEditing(true);
     }
@@ -231,6 +232,7 @@ const StudentModal = ({
       className={styles.modalOverlay}
       onClick={(e) => {
         if (e.target.className === styles.modalOverlay) {
+          console.log("Overlay clicked");
           handleClose(e);
         }
       }}
